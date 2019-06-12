31e2c721f03565b3784f2458a6d0796e8ae1f66b
Kevin Eze <kenware>
2019-06-12 19:27:59 +0100

    send a notification email to all users with before 24 hours

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'user',
-    'booking',
+    'booking.apps.BookingConfig',
     'flight',
     'django_nose',
     'corsheaders'
